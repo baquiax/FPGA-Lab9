3fae016aa041442fdc8d374055fbf554f4481b6b
Josué Alexander Baquiax Batén <alexanderbaquiax2009@gmail.com>
2015-04-26 13:47:27 -0600

    Files
    Project files

---
+++ JK.v
@@ -0,0 +1,30 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    18:33:01 04/20/2015 
+// Design Name: 
+// Module Name:    JK 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module JK(
+    input [1:0] J,
+    input [1:0] K,
+    output Q,
+    output Q',
+    input [1:0] C
+    );
+
+wire 
+endmodule
---
+++ Principal.v
@@ -0,0 +1,32 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    18:43:24 04/20/2015 
+// Design Name: 
+// Module Name:    Principal 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module Principal(
+    input SW0,
+    input BTN0,
+    input V,
+    output A,
+    output B
+    );
+wire w1, w2, w3, w4, w5, w6
+JK jk1()
+nor (w1, SWO, w4, )
+
+endmodule
